f9ee9fdec465615f41553634795ef6d9dd12cf5e
L0ver123 <165410925+L0ver123@users.noreply.github.com>
2024-04-29 19:31:31 +0300

    Create 12-2.py

---
+++ 12/12/12-2.py
@@ -0,0 +1,18 @@
+# Метод Ньютона
+
+def F(x):
+    return -3.456 + 5.76*x - 3.6*x**2 + x**3
+
+def F1(x):
+    return  5.76 - 6.2*x + 3*x**2
+
+x1 = float(input("Введите X0: "))
+eps = float(input("Введите Eps: "))
+x0 = x1 + 2*eps
+
+while abs(x1 - x0) > eps:
+    x0 = x1
+    x1 = x0 - F(x0) / F1(x0)
+
+print('X = ', x1)
+print('F(X) = ', F(x1))
